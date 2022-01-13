a4473a7a5a982de788afa1d44a0fb329bbf2fad3
Sarang Wadode <sarangwadode@gmail.com>
2022-01-13 19:01:19 +0530

    fix: after session ends break starts and vice versa

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,15 +8,16 @@ function App() {
   const [sessiontime, setSessiontime] = useState(25);
   const [timer, setTimer] = useState(25*60);
   const [start, setStart] = useState(false);
-  const [reset, setReset] = useState(false);
+  const [session, setSession] = useState('Session');
+  const [isSession,setIsSession] = useState(true);
 
 
   function handleClick(e){
-    if (e.target.id === "break-decrement" && breaktime > 0) {
+    if (e.target.id === "break-decrement" && breaktime > 1) {
       setBreaktime(breaktime - 1);
     } else if (e.target.id === "break-increment" && breaktime < 60) {
       setBreaktime(breaktime + 1);
-    } else if (e.target.id === "session-decrement" && sessiontime > 0) {
+    } else if (e.target.id === "session-decrement" && sessiontime > 1) {
       setSessiontime(sessiontime - 1);
     } else if (e.target.id === "session-increment" && sessiontime < 60) {
       setSessiontime(sessiontime + 1);
@@ -28,7 +29,7 @@ function App() {
       if (timer > 0 && start) {
         setTimer(timer - 1);
       }
-    },1000)
+    },200)
     return () => clearInterval(interval);
   },[timer,start])
 
@@ -36,14 +37,31 @@ function App() {
     setTimer(sessiontime*60);
   },[sessiontime])
 
+
+  function handleReset() {
+    setBreaktime(5);
+    setSessiontime(25);
+    setStart(false);
+    setIsSession(true);
+  }
+
   useEffect(() => {
-    if (reset === true) {
-      setSessiontime(25);
+    setSession(isSession ? 'Session' : 'Break');
+  }, [isSession])
+
+  useEffect(() => {
+    if( timer === 0 && isSession === true) {
+      setTimer(breaktime*60);
+      setStart(true);
+      setSession('Break');
+      setIsSession(false);
+    } else if (timer === 0 && isSession === false) {
       setTimer(sessiontime*60);
-      setStart(false);
-      setReset(false);
+      setStart(true);
+      setSession('Session');
+      setIsSession(true);
     }
-  },[reset,timer,sessiontime,start])
+  },[timer,breaktime,sessiontime,session,isSession])
 
 
   return (
@@ -56,13 +74,13 @@ function App() {
       </div>
 
       <div className="session">
-        <div id="timer-label">Session</div>
+        <div id="timer-label">{session}</div>
         <div id="time-left">{format_time(timer)}</div>
       </div>
 
       <div className="controls">
         <button id="start_stop" onClick={() => setStart(!start)}>Start</button>
-        <button id="reset" onClick={() => setReset(!reset)}>Reset</button>
+        <button id="reset" onClick={handleReset}>Reset</button>
       </div>
     </div>
   );
